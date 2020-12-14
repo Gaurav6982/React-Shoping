01463a18191116ea2180ab4af6a8d11e511f66df
Gaurav6982 <gaurav.jss.027@gmail.com>
2020-12-14 12:54:45 +0530

    Added REdux to Ffilter

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -1,4 +1,4 @@
-import { FETCH_PRODUCTS } from "../types";
+import { FETCH_PRODUCTS, ORDER_PRODUCTS_BY_SIZE, SORT_PRODUCTS_BY_PRICE } from "../types";
 
 export const fetchProducts = ()=>async(dispatch)=>{
     const res=await fetch("http://127.0.0.1:8000/api/products");
@@ -7,4 +7,41 @@ export const fetchProducts = ()=>async(dispatch)=>{
         type:FETCH_PRODUCTS,
         payload:data,
     });
+}
+
+export const filterProducts=(products,size)=>(dispatch)=>{
+    dispatch({
+        type:ORDER_PRODUCTS_BY_SIZE,
+        payload:{
+            size:size,
+            items:size===""?products:products.filter(x=>JSON.parse(x.availableSizes).indexOf(size)>=0),
+        }
+    })
+}
+
+export const sortProducts=(filteredProducts,sort)=>(dispatch)=>{
+    // let final;
+    var sortedProducts=filteredProducts.slice();
+    if(sort==="latest")
+    {
+        sortedProducts.sort((a,b)=>(a.id>b.id?1:-1));
+    }
+    else
+    {
+        if(sort==="lowest")
+        {
+            sortedProducts.sort((a,b)=>(a.price>b.price?1:-1));
+        }
+        else
+        {
+            sortedProducts.sort((a,b)=>a.price>b.price?-1:1);
+        }
+    }
+    dispatch({
+        type:SORT_PRODUCTS_BY_PRICE,
+        payload:{
+            sort:sort,
+            items:sortedProducts
+        }
+    })
 }
